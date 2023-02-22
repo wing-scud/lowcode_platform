57550c409db8f05ad1a843505df0ae3713d674a1
wing_scud <1205586003@qq.com>
2023-02-23 02:10:36 +0800

    feat: 初始化仓库
    使用umi搭建pc中台页面

---
+++ .umirc.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author: wing_scud 1205586003@qq.com
+ * @Date: 2023-02-23 01:50:22
+ * @LastEditors: wing_scud 1205586003@qq.com
+ * @LastEditTime: 2023-02-23 01:55:38
+ * @FilePath: \lowcode-platform\.umirc.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { defineConfig } from 'umi';
+import Route from "./config/route"
+export default defineConfig({
+  layout: {},
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes:Route,
+  fastRefresh: {},
+});
---
+++ config/route.ts
@@ -0,0 +1,5 @@
+export default [
+    { path: '/', component: '@/pages/Index/index', name: "首页" },
+    { path: '/create', component: '@/pages/Create/index', name: "搭建页" },
+    { path: '/demo', component: '@/pages/Demo/index', name: "页面示例" },
+]
